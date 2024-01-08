aa1c276a7149b3ed4f4022d2a19f08e363ad872c
obre-bjorn <obre.bjorn@gmail.com>
2024-01-08 12:33:44 +0300

    place ship view design

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
 // import { createPlayer,createComputerPlayer } from './Player/Player';
 import Game from './Game/Game';
 
+const gameContainer = document.getElementById('game-container')
+
+const board = document.createElement('div')
+
+board.classList.add('board')
+
+
+ for(let row = 0; row < 10; row+=1){
+    for (let col = 0; col < 10; col+=1){
+
+        const cell = document.createElement('div')
+                        cell.className = 'cell'
+                        cell.dataset.row = row
+                        cell.dataset.col = col
+                        cell.draggable = true
+
+                        cell.addEventListener('drop', (event) => {
+                            console.log(event.target.dataset.row)
+                        
+                        });
+
+                        board.appendChild(cell);
+
+                    }
+
+            }
 
 
 
-Game('Bjorn').init()
+
+// Game('Bjorn').init()
